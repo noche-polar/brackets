ba000eaeb5a1c91bd9da0b73c942c1429ab38f69
noche-polar <kastiuchik.elena@gmail.com>
2021-03-10 08:41:40 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 != 0) {return false;}
+
+  let array = str.split("");
+  
+  for (let i = 0; i < array.length; i++) {
+    for (let index in bracketsConfig) {
+      if (array[i] == bracketsConfig[index][0] && array[i + 1] == bracketsConfig[index][1]) {
+        array.splice(i, 2);
+        i = -1;
+      }
+    }
+  }
+
+  if (array.length == 0) {return true;}
+  return false;
 }
